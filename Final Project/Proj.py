4b2aa946dbb9f024d8e0ad77801c22dc04561c75
Yasamin Modabber <92112369+yasamino@users.noreply.github.com>
2024-04-24 11:04:37 -0400

    Update Proj.py

--- Final Project/Proj.py
+++ Final Project/Proj.py
@@ -22,7 +22,6 @@ import pandas as pd
 sys.path.append("C:\\Users\\lenovo\\Documents\\physics\\voronoi\\decfirst")
 import celldiv
 
-
 argv = sys.argv
 
 if "--" not in argv:
@@ -43,18 +42,10 @@ parser = argparse.ArgumentParser(description=helpString)
 parser.add_argument("trajPath", type=str,
                     help="Trajectory path. Absolute or relative.")
 
-parser.add_argument("-s", "--smooth", action='store_true',
-                    help="Do smoothing (really expensive and doesn't look as good)")
-
 parser.add_argument("-k", "--skip", type=int, required=False,
                     help="Trajectory frame skip rate. E.g. SKIP=10 will only \
                     render every 10th frame.",
                     default=1)
-
-parser.add_argument("-nc", "--noclear", type=bool, required=False,
-                    help="specifying this will not clear the destination directory\
-                    and restart rendering.",
-                    default=False)
 
 parser.add_argument("--min-cells", type=int, required=False,
                     help='Start rendering when system has at least this many cells',
@@ -68,36 +59,11 @@ parser.add_argument("-nf", "--num-frames", type=int, required=False,
                     help="Only render this many frames.",
                     default=sys.maxsize)
 
-parser.add_argument("-r", "--res", type=int, default=1, required=False,
-                    help='Renders images with resolution RES*1080p. RES>=1. \
-                    Use 2 for 4k. A high number will devour your RAM.')
-
-parser.add_argument("-cc", "--cell-color", type=int, nargs=3, required=False,
-                    default=[72, 38, 153],
-                    help="RGB values of cell color. From 0 to 255")
-
-parser.add_argument("-bc", "--background-color", type=int, nargs=3,
-                    required=False, default=[255,255,255],
-                    help="RGB values of cell color. From 0 to 255")
-
-parser.add_argument("-si", "--specular-intensity", type=float, required=False,
-                    default = 0.0,
-                    help="Set specular-intensity (shininess). From 0.0 to 1.0")
-
 args = parser.parse_args(argv)
 
 imageindex = 0
 firstfaces = []
 
-
-doSmooth = args.smooth
-if doSmooth:
-    print("Doing smoothing. Consider avoiding this feature...")
-
-
-if (args.res < 1):
-    print("ERROR: invalid resolution factor")
-    sys.exit()
 
 with open('C180_pentahexa.csv', newline='') as g:
     readerfaces = csv.reader(g, delimiter=',')
@@ -112,15 +78,6 @@ nSkip = args.skip
 
 if nSkip > 1:
     print("Rendering every %dth" % nSkip, "frame...")
-
-
-noClear = args.noclear
-
-sPath = os.path.splitext(filename)[0] + "/images/"
-
-if not noClear and os.path.exists(sPath):
-    for f in os.listdir(sPath):
-        os.remove(sPath+f)
 
 cellInds = []
 minInd = args.min_cells - 1
@@ -205,73 +162,7 @@ def Plot_area_distribution(vor,param):
     #svg
     #print(s)        
         #plt.scatter(vor.points[p][0], vor.points[p][1] , marker=".")
-        
-def Color_area_distribution(vor):
-    fig, ax = plt.subplots()
-    s=Calculte_area_distribution(vor)
-    Maximum=max(s) #find largest area - needed for sectioning 
-    #18 Jan
-    # Make a user-defined colormap.
-    cm1 = mcol.LinearSegmentedColormap.from_list("MyCmapName",["r","b"])
-    # Make a normalizer that will map the time values from
-    # [start_time,end_time+1] -> [0,1].
-    cnorm = mcol.Normalize(vmin=0,vmax=np.pi)
-    # Turn these into an object that can be used to map time values to colors and
-    # can be passed to plt.colorbar().
-    cpick = cm.ScalarMappable(norm=cnorm,cmap=cm1)
-    cpick.set_array([])
-
-    for p in range(len(vor.points)):
-        index_of_vor_region_for_point = vor.point_region[p]
-        if -1 not in vor.regions[index_of_vor_region_for_point]:
-            index_of_vertice = vor.regions[index_of_vor_region_for_point]
-            S_seg=0
-            A_seg=0
-            for v in range(len(index_of_vertice)): 
-                p1=vor.vertices[index_of_vertice[v]]
-                p2=vor.vertices[index_of_vertice[v-1]]
-                p3=vor.points[p]
-                S_seg += 1/2*np.abs(( p1[0]*(p2[1]-p3[1]) + p2[0]*(p3[1]-p1[1]) + p3[0]*(p1[1]-p2[1]) ))
-                A_seg += np.abs((p1[0]-p2[0]) + (p1[1]-p2[1])*1j)
-                # plt.show()
-                #plt.scatter(vor.vertices[ver][0],vor.vertices[ver][1])
-                #print(vor.vertices[ver])
-            
-            if S_seg<=(np.pi) and A_seg<2*np.pi:
-                p = []
-                for ind in index_of_vertice:
-                    p.append(vor.vertices[ind])
-                patches = []
-                patches.append(Polygon(p, closed=True))
-                p = PatchCollection(patches,edgecolor="r",alpha=0.8)
-                p.set_color(cpick.to_rgba(S_seg))
-                ax.add_collection(p)
-    #ax.autoscale()
-    ax.set_xlim([-5,45])
-    ax.set_ylim([-5,45])
-    
-    # F = plt.figure()
-    # A = F.add_subplot(111)
-    # for y, t in zip(ydat,tim):
-    #     A.plot(xdat,y,color=cpick.to_rgba(t))
-
-    plt.colorbar(cpick,label="Area",ax=plt.gca())
-    plt.savefig("Area_plot{}.png".format(i))
-    plt.close()
-
-    # fig.colorbar(ax=ax)
-    # plt.show()
-
-    # bottom = cm.get_cmap('Blues', 128)
-    # newcolors = np.vstack((bottom(np.linspace(0, 1, 128))))
-    # newcmp = ListedColormap(newcolors, name='OrangeBlue')
-
-    # sm = plt.cm.ScalarMappable(cmap=newcmp, norm=plt.Normalize(vmin=0, vmax=Maximum))
-    # sm.set_array([])
-    # plt.colorbar(sm,ax=ax)
-    #plt.show()
-    
-
+         
 def g3_distribution(com_coordinates,cut_off_radius): 
     r=[]
     cos_theta=[]
@@ -305,12 +196,11 @@ def g3_distribution(com_coordinates,cut_off_radius):
     cmap = cm.get_cmap('jet')
     plt.pcolormesh(xi, yi, zi.reshape(xi.shape), shading='gouraud', cmap=cmap)
     plt.colorbar()
-    #plt.title('Density plot of g3 distribution')
+    #plt.title('$\\gamma_{ext}$='+'{}'.format(1))
     plt.xlabel('r/$\\sigma$')
     plt.ylabel('$cos(\\theta)$')
     plt.savefig("g3_dist{}.png".format(cut_off_radius))
     plt.show()
-
 
 def save_g3_disrtibution(com_coordinates,cut_off_radius,filename,header,h):
     r=[]
@@ -350,15 +240,87 @@ def save_g3_disrtibution(com_coordinates,cut_off_radius,filename,header,h):
     df.to_csv(filename+".csv", index=False)
     # plot and save the results
     cmap = cm.get_cmap('jet')
+    #clear previous plot
+    plt.clf()
+    
     plt.pcolormesh(xi, yi, zi.reshape(xi.shape), shading='gouraud', cmap=cmap)
     plt.colorbar()
     #plt.title('Density plot of g3 distribution')
     plt.xlabel('r/$\\sigma$')
     plt.ylabel('$cos(\\theta)$')
+    #save photo in file photos
+    
     plt.savefig("g3_dist{}.png".format(h))
 
+def Color_area_distribution(coords,scan_theta,scan_radius): # (coords,[theta_min,theta_max],[r_min,r_max])
+    vor=Voronoi(coords)
+    fig, ax = plt.subplots()
+    s=Calculte_area_distribution(vor)
+    Maximum=scan_theta[1] #largest theta 
+    cm1 = mcol.LinearSegmentedColormap.from_list("MyCmapName",["r","b"])
+    cnorm = mcol.Normalize(vmin=0,vmax=np.pi)
+    cpick = cm.ScalarMappable(norm=cnorm,cmap=cm1)
+    cpick.set_array([])
 
+    for p in range(len(vor.points)):
+        index_of_vor_region_for_point = vor.point_region[p]
+        if -1 not in vor.regions[index_of_vor_region_for_point]:
+            index_of_vertice = vor.regions[index_of_vor_region_for_point]
+            #calculte the area 
+            S_seg=0
+            A_seg=0
+            for v in range(len(index_of_vertice)): 
+                p1=vor.vertices[index_of_vertice[v]]
+                p2=vor.vertices[index_of_vertice[v-1]]
+                p3=vor.points[p]
+                S_seg += 1/2*np.abs(( p1[0]*(p2[1]-p3[1]) + p2[0]*(p3[1]-p1[1]) + p3[0]*(p1[1]-p2[1]) ))
+                A_seg += np.abs((p1[0]-p2[0]) + (p1[1]-p2[1])*1j)
 
+            #g3
+            B=vor.points[p]
+            com_coordinates = coords
+            other_coordinates = [i for i in com_coordinates if not np.all(i == B)]
+            dist = [np.linalg.norm(np.array(B) - np.array(i)) for i in other_coordinates]
+            #find min index
+            min_index = dist.index(min(dist))
+            A = other_coordinates[min_index]
+            other_other_coordinates= [i for i in other_coordinates if not np.all(i == A)]
+            #find the atom C in the cutoff range
+            C = [i for i in other_other_coordinates if np.linalg.norm(np.array(B) - np.array(i)) < scan_radius[1]]
+            if C:
+                #calculate the angle between the vectors AB and AC
+                AB = np.array(B) - np.array(A)
+                AC = np.array(B) - np.array(C)
+                r=[]
+                cos_theta=[]
+                for i in range(len(AC)):
+                    r.append(np.linalg.norm(AC[i]))
+                    cos_theta.append(np.dot(AB,AC[i])/(np.linalg.norm(AB)*np.linalg.norm(AC[i])))
+                
+                #if a cos(theta) is in the range of the scan
+                if any(scan_theta[0]<i<scan_theta[1] for i in cos_theta) and any(scan_radius[0]<i<scan_radius[1] for i in r) and S_seg<=(np.pi):
+                    p = []
+                    for ind in index_of_vertice:
+                        p.append(vor.vertices[ind])
+                    patches = []
+                    patches.append(Polygon(p, closed=True))
+                    p = PatchCollection(patches,edgecolor="r",alpha=0.8)
+                    p.set_color(cpick.to_rgba(S_seg))
+                    ax.add_collection(p)
+            elif S_seg<=(np.pi): 
+                p = []
+                for ind in index_of_vertice:
+                    p.append(vor.vertices[ind])
+                patches = []
+                patches.append(Polygon(p, closed=True))
+                p = PatchCollection(patches,edgecolor="b",alpha=0.4)
+                p.set_color("black")
+                ax.add_collection(p)
+    ax.set_xlim([-5,45])
+    ax.set_ylim([-5,45])
+    plt.colorbar(cpick,label="Area",ax=plt.gca())
+    plt.savefig("Area_plot{}.png".format(i))
+    plt.close()
 
 
 file_number=1
@@ -378,12 +340,12 @@ for filename in Files:
                 for mi in range(int(len(f)/192)):
                     cell_com.append(np.mean(f[mi*192:(mi+1)*192],axis=0))
                 coords= np.array(cell_com)[: , 0:2] #2D coordinates
-                if frameCount==int(th.maxFrames/nSkip):
-                    #Color_area_distribution(Voronoi(coords))    
+                if frameCount==int(th.maxFrames/nSkip):  
                     #save_g3_disrtibution(coords,2,"g3_dist","visc",file_number)
                     print(file_number)
-                    g3_distribution(coords,2)
-                    Plot_cell_distribution(coords)
+                    #g3_distribution(coords,2)
+                    #Plot_cell_distribution(coords)
+                    Color_area_distribution(coords,[-1,1],[0,1.0])
 
         except celldiv.IncompleteTrajectoryError:
             print ("Stopping...")   
